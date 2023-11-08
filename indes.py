df6aff77163103f23fd765bd3bc92dd9b92fa6ca
bekasmart <149644433+bekasmart@users.noreply.github.com>
2023-11-08 09:15:16 +0600

    mahmutjon

---
+++ indes.py
@@ -0,0 +1,23 @@
+concatenated_string = ' '.join(['Python', 'is', 'a', 'powerful', 'language'])
+
+topic = "strings in Python."
+
+print('"Learning about \\"Python\\" strings is fun."')
+
+topic_length = len(topic)
+s_count = topic.count('s')
+
+upper_topic = topic.upper()
+lower_topic = topic.lower()
+
+formatted_string = f"Let's learn about {topic}"
+
+extracted_word = topic[10:16]
+
+contains_python = 'Python' in topic
+
+modified_topic = topic.replace('Python', 'programming')
+
+programming_languages = 'Python,Java,C++,Ruby'.split(',')
+
+char_at_index_4 = topic[4]
